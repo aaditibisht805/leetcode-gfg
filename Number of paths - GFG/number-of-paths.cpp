1d854f2818905711e23eb386001e422b9597bea4
Aaditi Bisht <107092979+aaditibisht805@users.noreply.github.com>
2023-09-07 17:56:33 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.cpp
+++ Number of paths - GFG/number-of-paths.cpp
@@ -6,23 +6,26 @@ using namespace std;
 // } Driver Code Ends
 
 
-long long  solve(int n, int m , int i, int j)
+long long  solve(int n, int m , int i, int j, vector<vector<long long>>&dp)
 {
     if(i==n-1 && j==m-1)
     return 1;
     if(i>=n || j >=m)
     return 0;
+    if(dp[i][j]!=-1)
+    return dp[i][j];
     
-    long long right=solve(n, m, i, j+1);
-    long long bot =solve(n, m, i+1, j);
+    long long right=solve(n, m, i, j+1, dp);
+    long long bot =solve(n, m, i+1, j, dp);
     long long sum = right + bot;
-    return sum;
+    return dp[i][j]=sum;
 }
 long long  numberOfPaths(int m, int n)
 { int i =0;
   int j =0;
+  vector<vector<long long>>dp(n, vector<long long>(m, -1));
   
-   long long res = solve(n, m, i, j);
+   long long res = solve(n, m, i, j, dp);
    return res;
 }
 
